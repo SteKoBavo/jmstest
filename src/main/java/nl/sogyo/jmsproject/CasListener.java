05a9c016d0d7c117ed7421852369ffca82efa2fa
skorenberg <skorenberg@sogyo.nl>
2017-05-30 17:05:03 +0200

    refactor

--- src/main/java/nl/sogyo/jmsproject/CasListener.java
+++ src/main/java/nl/sogyo/jmsproject/CasListener.java
@@ -1,38 +1,49 @@
 package nl.sogyo.jmsproject;
 
-// Cassandra Java Driver
 import com.datastax.driver.core.*;
-
-// JMS
 import javax.jms.*;
 
 public class CasListener {
+	private JMSTopic jmsTopic;
+	private MessageConsumer consumer;
+	private Cluster cluster;
+	private com.datastax.driver.core.Session casSession;
+	
+	public CasListener() throws JMSException {
+		// Connect to ActiveMQ Topic
+		this.jmsTopic = new JMSTopic("localhost",61616,"admin","password","event");
+		this.consumer = jmsTopic.getConsumer();
+		
+		// Connect to the Cassandra cluster and keyspace "dev"
+		this.cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+		this.casSession = cluster.connect("dev");
+	}
+	
+	public void listen() throws JMSException {
+		while(true) {
+			Message msg = this.consumer.receive();
+			this.onMessage(msg);
+		}		
+	}
+
+	public void onMessage(Message msg) throws JMSException {
+		String body = ((TextMessage) msg).getText();
+		String[] row = body.split(";");
+		String query = "INSERT INTO people (firstname, lastname) VALUES ('" + row[0] + "', '" + row[1] + "')";
+		this.casSession.execute(query);
+	}
+	
+	public void close() throws JMSException {
+		this.casSession.close();
+		this.cluster.close();
+		this.jmsTopic.close();
+	}
+	
 	public static void main (String[] args) {
 		try {
-			// Connect to ActiveMQ
-			JMSTopic jmsTopic = new JMSTopic("localhost",61616,"admin","password","event");
-			MessageConsumer consumer = jmsTopic.getConsumer();
-			
-			// Connect to the Cassandra cluster and keyspace "dev"
-			Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
-			com.datastax.driver.core.Session casSession = cluster.connect("dev");
-			
-			// Listen to the topic and INSERT rows
-			while(true) {
-				Message msg = consumer.receive();
-				String body = ((TextMessage) msg).getText();
-				String[] row = body.split(";");
-				if (row.length != 2) {
-					break;
-				}
-				String query = "INSERT INTO people (firstname, lastname) VALUES ('" + row[0] + "', '" + row[1] + "')";
-				casSession.execute(query);
-			}
-			
-			// Close the connections
-			casSession.close();
-			cluster.close();
-			jmsTopic.close();
+			CasListener casListener = new CasListener();
+			casListener.listen();
+			casListener.close();
         } catch (Exception e) {
 			// Just printStackTrace for all Exceptions for simplicity
             e.printStackTrace();
--- src/main/java/nl/sogyo/jmsproject/PgPublisher.java
+++ src/main/java/nl/sogyo/jmsproject/PgPublisher.java
@@ -1,61 +1,72 @@
 package nl.sogyo.jmsproject;
 
-// JDBC
 import java.sql.*;
-
-// JMS
 import javax.jms.*;
 
 public class PgPublisher {
+	JMSTopic jmsTopic;
+	MessageProducer producer;
+	java.sql.Connection pgConnection;
+	org.postgresql.PGConnection listenConnection;
+	
+	public PgPublisher() throws Exception {
+		// Connect to ActiveMQ
+		this.jmsTopic = new JMSTopic("localhost",61616,"admin","password","event");
+		this.producer = this.jmsTopic.getProducer();
+		
+		// Connect to PostGreSQL
+		Class.forName("org.postgresql.Driver");
+		this.pgConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "stefan", "password");
+		
+		// Listen to PostgreSQL channel
+		this.listenConnection = (org.postgresql.PGConnection) this.pgConnection;
+		Statement stmt = this.pgConnection.createStatement();
+		stmt.execute("LISTEN mychannel");
+		stmt.close();
+	}
+	
+	public void publish() throws Exception {
+		while (true) {
+			Statement stmt = this.pgConnection.createStatement();
+			ResultSet rst = stmt.executeQuery("SELECT 1");	//Issue a dummy query to contact the backend and receive any pending notifications.
+			rst.close();
+			stmt.close();
+
+			org.postgresql.PGNotification notifications[] = this.listenConnection.getNotifications();
+			if (notifications != null) {
+				System.out.println("Got notification!");	//Note that we cannot (yet) delete records from the Cassandra DB this way, only add new records. Also, Cassandra automatically overwrites a record when a row is inserted with the same primary key.
+				
+				// Query the TABLE testschema.test
+				Statement st = this.pgConnection.createStatement();
+				ResultSet rs = st.executeQuery("SELECT * FROM testschema.test");
+				
+				// Publish all rows
+				while (rs.next()) {
+					TextMessage msg = this.jmsTopic.createTextMessage(rs.getString(1).trim() + ";" + rs.getString(2).trim());
+					this.producer.send(msg);
+				}
+				
+				rs.close();
+				st.close();
+			}
+			if (false) {
+				break;
+			}
+	
+			Thread.sleep(500);
+		}
+	}
+	
+	public void close() throws Exception {
+		this.pgConnection.close();
+		this.jmsTopic.close();
+	}
+	
 	public static void main (String[] args) {
 		try {
-			// Connect to ActiveMQ
-			JMSTopic jmsTopic = new JMSTopic("localhost",61616,"admin","password","event");
-			MessageProducer producer = jmsTopic.getProducer();
-			
-			// Connect to PostGreSQL
-			Class.forName("org.postgresql.Driver");
-			java.sql.Connection pgConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/testdb", "stefan", "password");
-			
-			// Listen to PostgreSQL channel
-			org.postgresql.PGConnection listenConnection = (org.postgresql.PGConnection) pgConnection;
-			Statement stmt = pgConnection.createStatement();
-			stmt.execute("LISTEN mychannel");
-			stmt.close();
-			
-			while (true) {
-				stmt = pgConnection.createStatement();
-				ResultSet rst = stmt.executeQuery("SELECT 1");	//Issue a dummy query to contact the backend and receive any pending notifications.
-				rst.close();
-				stmt.close();
-
-				org.postgresql.PGNotification notifications[] = listenConnection.getNotifications();
-				if (notifications != null) {
-					System.out.println("Got notification!");	//Note that we cannot (yet) delete records from the Cassandra DB this way, only add new records. Also, Cassandra automatically overwrites a record when a row is inserted with the same primary key.
-					
-					// Query the TABLE testschema.test
-					Statement st = pgConnection.createStatement();
-					ResultSet rs = st.executeQuery("SELECT * FROM testschema.test");
-					
-					// Publish all rows
-					while (rs.next()) {
-						TextMessage msg = jmsTopic.createTextMessage(rs.getString(1).trim() + ";" + rs.getString(2).trim());
-						producer.send(msg);
-					}
-					
-					rs.close();
-					st.close();
-				}
-				if (false) {
-					break;
-				}
-	
-				Thread.sleep(500);
-			}
-			
-			// Close the connections
-			pgConnection.close();
-			jmsTopic.close();
+			PgPublisher pgPublisher = new PgPublisher();
+			pgPublisher.publish();
+			pgPublisher.close();
         } catch (Exception e) {
 			// Just printStackTrace for all Exceptions for simplicity
             e.printStackTrace();
